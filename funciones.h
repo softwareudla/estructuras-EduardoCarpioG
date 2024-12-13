1a986453442123c4a91e355133bf0b0d66cb06aa
EduardoCarpioG <156716249+EduardoCarpioG@users.noreply.github.com>
2024-12-13 08:30:46 -0500

    Add files via upload

--- funciones.h
+++ funciones.h
@@ -2,16 +2,19 @@
 #define FUNCIONES_H
 #define MAX_LIBROS 20
 typedef struct {
-    int id;          
-    char titulo[100];     
-    char autor[50];     
-    int anio;            
-    char estado[20];    
+    int id;             
+    char titulo[100];  
+    char autor[50];    
+    int anio;             
+    char estado[20];     
 } Libro;
+
 void inicializarBiblioteca(Libro biblioteca[], int *cantidad);
 void registrarLibro(Libro biblioteca[], int *cantidad);
 void mostrarLibros(Libro biblioteca[], int cantidad);
 void buscarLibro(Libro biblioteca[], int cantidad);
 void actualizarEstado(Libro biblioteca[], int cantidad);
 void eliminarLibro(Libro biblioteca[], int *cantidad);
-#endif 
+int idUnico(Libro biblioteca[], int cantidad, int id);
+
+#endif
